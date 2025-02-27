86219d51df12ec9cbc1aaa967b76c77949501463
chananchida008 <chananchida.klin@bumail.net>
2025-02-27 21:31:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, BU 1660702216'
 
-if __name__ == '__main__':
-    app.run(debug=True, port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
